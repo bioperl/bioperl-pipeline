3078d00a54c1b5a5b22892244975280c75f24f15
shawnh <shawnh@bioperl.org>
2002-07-26 09:34:23 +0000

    test data files for pipeline
    svn path=/bioperl-pipeline/trunk/; revision=12297

---
+++ t/data/init.sql
@@ -0,0 +1,109 @@
+# MySQL dump 8.13
+#
+# Host: localhost    Database: test_run
+#--------------------------------------------------------
+# Server version	3.23.36
+
+#
+# Dumping data for table 'analysis'
+#
+
+INSERT INTO analysis VALUES (1,'2002-07-26 16:22:45','blast','Bio::Pipeline::Runnable::TestRunnable',NULL,NULL,NULL,'testrun',NULL,NULL,'test',NULL,NULL,0);
+INSERT INTO analysis VALUES (2,'2002-07-26 16:22:46','blast','Bio::Pipeline::Runnable::TestRunnable',NULL,NULL,NULL,'testrun',NULL,NULL,'test',NULL,NULL,0);
+
+#
+# Dumping data for table 'analysis_output_handler'
+#
+
+INSERT INTO analysis_output_handler VALUES (1,2);
+INSERT INTO analysis_output_handler VALUES (2,2);
+
+#
+# Dumping data for table 'argument'
+#
+
+INSERT INTO argument VALUES (1,1,'-file','t/data/testin.fa',1,'SCALAR');
+INSERT INTO argument VALUES (2,1,'-format','Fasta',2,'SCALAR');
+INSERT INTO argument VALUES (3,3,'-file','>t/data/testout.fa',1,'SCALAR');
+INSERT INTO argument VALUES (4,3,'-format','Fasta',2,'SCALAR');
+INSERT INTO argument VALUES (5,4,NULL,'OUTPUT',1,'SCALAR');
+
+#
+# Dumping data for table 'completed_jobs'
+#
+
+
+#
+# Dumping data for table 'datahandler'
+#
+
+INSERT INTO datahandler VALUES (1,1,'new',1);
+INSERT INTO datahandler VALUES (2,1,'next_seq',2);
+INSERT INTO datahandler VALUES (3,2,'new',1);
+INSERT INTO datahandler VALUES (4,2,'write_seq',1);
+
+#
+# Dumping data for table 'dbadaptor'
+#
+
+
+#
+# Dumping data for table 'input'
+#
+
+INSERT INTO input VALUES (1,'test1',1,1);
+INSERT INTO input VALUES (2,'test2',2,1);
+
+#
+# Dumping data for table 'iohandler'
+#
+
+INSERT INTO iohandler VALUES (1,1,'INPUT','STREAM');
+INSERT INTO iohandler VALUES (2,1,'OUTPUT','STREAM');
+
+#
+# Dumping data for table 'job'
+#
+
+INSERT INTO job VALUES (1,'NEW',1,0,'/tmp//5/test_run.job_1.blast.1027671765.239.out','/tmp//5/test_run.job_1.blast.1027671765.239.err','/tmp//5/test_run.job_1.blast.1027671765.239.obj','NEW','','2002-07-26 16:22:46',0);
+INSERT INTO job VALUES (2,'NEW',1,0,'/tmp//1/test_run.job_2.blast.1027671765.819.out','/tmp//1/test_run.job_2.blast.1027671765.819.err','/tmp//1/test_run.job_2.blast.1027671765.819.obj','NEW','','2002-07-26 16:22:46',0);
+
+#
+# Dumping data for table 'new_input'
+#
+
+
+#
+# Dumping data for table 'new_input_ioh'
+#
+
+
+#
+# Dumping data for table 'node'
+#
+
+INSERT INTO node VALUES (1,'59',1);
+
+#
+# Dumping data for table 'node_group'
+#
+
+INSERT INTO node_group VALUES (1,'gr1','desc1');
+
+#
+# Dumping data for table 'output'
+#
+
+
+#
+# Dumping data for table 'rule'
+#
+
+INSERT INTO rule VALUES (1,1,2,'WAITFORALL_AND_UPDATE');
+
+#
+# Dumping data for table 'streamadaptor'
+#
+
+INSERT INTO streamadaptor VALUES (1,'Bio::SeqIO');
+
