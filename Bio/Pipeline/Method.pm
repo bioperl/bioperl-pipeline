9bc2ee46e501b0782b540f2c4157870593252964
shawnh <shawnh@bioperl.org>
2003-02-20 12:08:19 +0000

    Added docs
    svn path=/bioperl-pipeline/trunk/; revision=12726

--- Bio/Pipeline/Method.pm
+++ Bio/Pipeline/Method.pm
@@ -1,5 +1,53 @@
+# BioPerl module for Bio::Pipeline::Method
+#
+# You may distribute this module under the same terms as perl itself
+#
+# POD documentation - main docs before the code
+#
 
+=head1 NAME
 
+Bio::Pipeline::Method 
+
+=head1 SYNOPSIS
+  my $data_handler = Bio::Pipeline::Method->new(-dbid=>1,
+                                                   -method=>"fetch_by_dbID,                                                   -argument=>\@arguments,
+                                                   -rank=>1);
+=head1 DESCRIPTION 
+Methods specifiy the adaptor methods and the correspoding arguments needed by a IO to fetch or store output. 
+The rank represents the order in which they are called by IOHandler methods
+fetch_input, write_output
+
+=head1 FEEDBACK
+
+=head2 Mailing Lists
+
+User feedback is an integral part of the evolution of this and other
+Bioperl modules. Send your comments and suggestions preferably to one
+of the Bioperl mailing lists.  Your participation is much appreciated.
+
+  bioperl-pipeline@bioperl.org          - General discussion
+  http://bio.perl.org/MailList.html     - About the mailing lists
+
+=head2 Reporting Bugs
+
+Report bugs to the Bioperl bug tracking system to help us keep track
+the bugs and their resolution.  Bug reports can be submitted via email
+or the web:
+
+  bioperl-bugs@bio.perl.org
+  http://bugzilla.bioperl.org/
+
+=head1 AUTHOR - FuguI Team
+
+Email fugui@fugu-sg.org
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal metho
+ds are usually preceded with a _
+
+=cut
 
 # Let the code begin...
 
@@ -15,13 +63,13 @@ use Bio::Root::Root;
 =head2 new
 
   Title   : new
-  Usage   : my $data_handler = Bio::Pipeline::DataHandler->new(-dbid=>1,
+  Usage   : my $data_handler = Bio::Pipeline::Method->new(-dbid=>1,
                                                    -method=>"fetch_by_dbID",
                                                    -argument=>\@arguments,
                                                    -rank=>1);
   Function: this constructor should only be used in the IO_adaptor or IO objects.
-            generates a new Bio::Pipeline::DataHandler
-  Returns : a new DataHandler object
+            generates a new Bio::Pipeline::Method
+  Returns : a new Method object
   Args    :
 
 =cut
@@ -34,14 +82,8 @@ sub new {
                                                                ARGUMENT
                                                                RANK)],@args);
 
-#  $dbID || $self->throw("DataHandler constructor needs a dbID");
-#  $rank || $self->throw("datahandle constructor needs a rank");
-  $name || $self->throw("datahandler needs a method arg.");
+  $name || $self->throw("Method needs a method arg.");
 
-# arguments may not be assigned when the method is constructed. 
-#  unless(ref($argument) eq 'ARRAY'){
-#        $self->throw("DataHandler expects argument as a reference of an array");
-# }
   $dbID && $self->dbID($dbID);
   $self->{'_name'} = $name;
   $self->{'_argument'} = $argument;
@@ -73,8 +115,8 @@ sub dbID {
 
   Title    : method
   Function : returns method
-  Example  : $datahandler->method();
-  Returns  : method of the datahandler
+  Example  : $Method->method();
+  Returns  : method of the Method
   Args     :
 
 =cut
@@ -92,26 +134,14 @@ sub name{
   Title    : argument
   Function : returns argument
   Example  : $dh->adpator_arg();
-  Returns  : argument of the datahandler
+  Returns  : argument of the Method
   Args     :
 
 =cut
 
-sub add_argument{
-	my ($self, $argument) = @_;
-	$argument || $self->throw("need a argument");
-	push @{$self->{'_argument'}}, $argument;
-}
-
 sub arguments{
     my ($self) = @_;
     return $self->{'_argument'};
-}
-
-
-sub flush_arguments {
-	my ($self) =  @_;
-	@{$self->{'_argument'}} = ();
 }
 
 =head2 rank
@@ -119,7 +149,7 @@ sub flush_arguments {
   Title    : rank
   Function : returns rank
   Example  : $dh->rank();
-  Returns  : rank the datahandler
+  Returns  : rank the Method
   Args     :
 
 =cut
